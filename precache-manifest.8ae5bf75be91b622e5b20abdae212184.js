2438dcc7aa9da7fba78dead2ededafa65fce5dea
Anton <bazhan.anton@ukr.net>
2020-07-16 17:49:33 +0300

    Updates

---
+++ precache-manifest.8ae5bf75be91b622e5b20abdae212184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf88dca9ad3013a857726f9a5f26f0ef",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "30895b20ce6be648ff48",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b481dd72637f10b838c8",
+    "url": "/goit-react-hw-02-feedback/static/js/main.94dfd488.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
